721d2a147bb2c2c6139423d99e6054d0462c1da9
Johan Mickos <johan.mickos@gmail.com>
2018-08-15 14:16:14 +0200

    Initialize project as 'petty'

---
+++ README.md
@@ -0,0 +1,3 @@
+# petty
+A poor man's implementation of a Netty-like selector-driven event loop in Rust.
+
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#[macro_use]
+extern crate futures;
+extern crate udt;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn sanity() {
+        assert_eq!(1, 1);
+    }
+}
